f3ca17e7cb1e94871b0ee0ce7c80832b3f9a8bff
Sashok9203 <kovalchuk.olex@gmail.com>
2024-06-05 00:17:40 +0300

    Update

--- user_creation_form.php
+++ user_creation_form.php
@@ -8,7 +8,6 @@
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.min.css">
     <link rel ="stylesheet" href="css/bootstrap.min.css">
     <link rel ="stylesheet" href="css/user_creation_form.css">
-
     <title>Новий користувач</title>
 </head>
 <body>
